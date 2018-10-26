6cdbf9b916c2e80f77dc30666df61502bc867907
Ryan Smith <ryan.l.smith4@gmail.com>
2018-10-26 16:02:24 -0700

    finished madLibs

--- stories.py
+++ stories.py
@@ -1,53 +1,55 @@
-#Empty Space for show_story
+
 fresh_prince = '''Now this is the story all about how
-My life got flipped, turned upside down
-And Id like to take a minute just sit right there
-Ill tell you how I became the prince of a town called Bel-air'''
-fresh_prince1 = '''In west Philadelphia born and raised
-On the playground where I spent most of my days
+______ life got flipped, ______ upside down
+And ______ like to take a minute just sit right there
+Ill tell you how ______ became the prince of a town called _____!'''
+
+fresh_prince1 = '''In west ______ born and raised
+On the playground where ______ spent most of ______ days
 Chilling out, maxing, relaxing all cool
-And all shooting some b-ball outside of the school
+And all ______ some b-ball outside of the school
 When a couple of guys, they were up to no good
-Started making trouble in my neighbourhood
-I got in one little fight and my mom got scared
-And said Youre moving with your auntie and uncle in Bel-air'''
+Started making trouble in______ neighbourhood
+______ got in one little fight and ______ mom got scared
+And said Youre moving with your auntie and uncle in ______!'''
 
-fresh_prince2 = '''I whistled for a cab and when it came near the
+fresh_prince2 = '''______ whistled for a cab and when it came near the
 License plate said fresh and had a dice in the mirror
-If anything I could say that this cab was rare
-But I thought nah, forget it, yo homes to Bel-air!'''
+If anything ______ could say that this cab was rare
+But ______ thought nah, ______ it, yo homes to _____!'''
 
-fresh_prince3 = '''I pulled up to a house about seven or eight
-And I yelled to the cabby Yo, homes smell you later!
-Looked at my kingdom I was finally there
-To sit on my throne as the prince of Bel-air'''
+fresh_prince3 = '''______ pulled up to a house about seven or eight
+And ______ yelled to the cabby Yo, homes ______ you later!
+Looked at ______ kingdom ______ was finally there
+To sit on ______ throne as the ______ of _____'''
 
 ###############################################################################
-#Variable Arrangements for 
+#Variable Arrangements for
 fresh_prince4 = '''Now this is the story all about how
-My life got flipped, turned upside down
-And Id like to take a minute just sit right there
-Ill tell you how I became the prince of a town called Bel-air'''
-fresh_prince5 = '''In west Philadelphia born and raised
-On the playground where I spent most of my days
+{0} life got flipped, {1} upside dow
+And {3} like to take a minute just sit right there
+Ill tell you how {0} became the {3}of a town called {2}!'''
+
+fresh_prince5 = '''In west {2} born and raised
+On the playground where {0} spent most of {3} days
 Chilling out, maxing, relaxing all cool
-And all shooting some b-ball outside of the school
+And all {1} some b-ball outside of the school
 When a couple of guys, they were up to no good
-Started making trouble in my neighbourhood
-I got in one little fight and my mom got scared
-And said Youre moving with your auntie and uncle in Bel-air'''
+Started making trouble in {3} neighbourhood
+{0} got in one little fight and {3} mom got scared
+And said Youre moving with your auntie and uncle in {2}!'''
 
-fresh_prince6 = '''I whistled for a cab and when it came near the
+fresh_prince6 = '''{0} whistled for a cab and when it came near the
 License plate said fresh and had a dice in the mirror
-If anything I could say that this cab was rare
-But I thought nah, forget it, yo homes to Bel-air!'''
+If anything {0} could say that this cab was rare
+But {0} thought nah, {1} it, yo homes to {2}!'''
 
-fresh_prince7 = '''I pulled up to a house about seven or eight
-And I yelled to the cabby Yo, homes smell you later!
-Looked at my kingdom I was finally there
-To sit on my throne as the prince of Bel-air'''
+fresh_prince7 = '''{0} pulled up to a house about seven or eight
+And {0} yelled to the cabby Yo, homes {1} you later!
+Looked at {3} kingdom {0} was finally there
+To sit on {0} throne as the prince of {2}'''
 
 
-stories= [fresh_prince4, fresh_prince5, fresh_prince6, fresh_prince7]
+stories_mad = [fresh_prince4, fresh_prince5, fresh_prince6, fresh_prince7]
 
-stories= [fresh_prince, fresh_prince1, fresh_prince2, fresh_prince3]
+stories = [fresh_prince, fresh_prince1, fresh_prince2, fresh_prince3]
--- story.py
+++ story.py
@@ -1,27 +1,43 @@
 from stories import stories
+from stories import stories_mad
+from os import system, name
+
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+#storing the madlib variables in the dict
 words = {
-"noun": '',
-"adjective": '',
 "verb": '',
-"place": ''
+"place": '',
+"name": '',
+"pronoun": '',
+"option": ''
 }
+#end dict
 
-
+# method to take input
 def user_input(prompt):
     user_input = input(prompt)
     return user_input
+#end user input
 
-
+#Method taking the noun, adj, verb, and place
 def make_story():
-    noun = user_input('Please give me a noun: ')
-    words["noun"] = noun
-    adjective = user_input("Please give me an adjective: ")
-    words["adjective"] = adjective
+    name = user_input('Please give me a name: ')
+    words["name"] = name
+    pronoun = user_input('Please give me a pronoun:  ')
+    words["pronoun"] = pronoun
     verb = user_input("Please give me a verb: ")
     words["verb"] = verb
     place = user_input("Please specify a place: ")
     words["place"] = place
+    return(words)
+#end make_story
 
+#method for user selection to print story before they madlib it
 def select(option):
     if option == 1:
         print(stories[0])
@@ -29,15 +45,33 @@ def select(option):
         print(stories[1])
     elif option == 3:
         print(stories[2])
+    elif option == 4:
+        print(stories[3])
     else:
         print("not a story in our stories :( ")
+#end select
 
+#method to actually show the story
 def show_story():
     make_story()
-    print(stories[0].format(words.get("noun"), words.get('verb'), words.get("place")))
+
+    print(stories_mad[int(words.get("option")) -1].format(words.get("name"), words.get("verb"), words.get("place"), words.get("pronoun")))
 
 
-selection = int(user_input("select an story 1 - 3: "))
-select(selection)
-# show_story()
-# def test():
+game = True
+while game:
+    try:
+        selection = int(user_input("select an story 1 - 4: "))
+        select(selection)
+        result = user_input("Did you want to madPrince that one? y/n: ").upper()
+        if result == 'Y':
+            words["option"] = selection
+            clear()
+            show_story()
+            play_again = user_input("Wanna play again? y/n: ").upper()
+            if play_again == 'Y':
+                pass
+            else:
+                game = False
+    except:
+        print("Use an Integer")
